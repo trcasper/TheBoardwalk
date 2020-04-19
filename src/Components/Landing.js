7651ea906ebd2938da660a7864b9ba3d0da724c9
trcasper <trcasper90@gmail.com>
2020-04-19 12:02:17 -0600

    sizing test

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import './Landing.scss';
+import React from "react";
+import "./Landing.scss";
 
 class Landing extends React.Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <div className="Landing">Landing</div>
-        )
-    }
+  render() {
+    return (
+      <div className="Landing">
+        <div id="Intro">
+
+          <div className="Nav">
+            <div id="HomeLink">Home</div>
+            <div id="AboutLink">About</div>
+            <div id="InsideLink">Inside</div>
+            <div id='ContactLink'>Contact</div>
+            <div id='EventsLink'>Events</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Landing;
+
+
+{/* <div className="TestText">This is a test to see how retarded
+<br/>i am when it comes to adjusting the sizes for this bull
+<br/>anything anything anything anything anything anything
+<br/>Test Test Test</div> */}
