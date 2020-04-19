cbe20f14b1bacd3f27747c246af86bc036004b96
trcasper <trcasper90@gmail.com>
2020-04-19 11:32:17 -0600

    basic setup

---
+++ src/Components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Landing.scss';
+
+class Landing extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="Landing">Landing</div>
+        )
+    }
+}
+
+export default Landing;
